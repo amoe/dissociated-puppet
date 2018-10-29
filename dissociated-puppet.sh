ab1e794f7ac97e98094702db53c9204b2e17a03a
David Banks <amoebae@gmail.com>
2018-10-29 19:51:01 +0000

    remove command

--- dissociated-puppet.sh
+++ dissociated-puppet.sh
@@ -4,5 +4,3 @@ puppet_root=/opt/dissociated-puppet
 ruby_path="${puppet_root}/lib/ruby/site_ruby"
 
 "${puppet_root}/bin/ruby" -I"$ruby_path" "${puppet_root}/bin/puppet" config print
-
-"${puppet_root}/bin/ruby" -I"$ruby_path" "${puppet_root}/bin/puppet" "$@"
