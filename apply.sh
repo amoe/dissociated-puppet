527428d4da4e3a7e9da7ae762797db84b7693ed6
David Banks <amoebae@gmail.com>
2018-11-29 09:52:46 +0000

    debug info

--- apply.sh
+++ apply.sh
@@ -5,5 +5,5 @@ confdir="${puppet_root}/etc/puppet"
 ruby_path="${puppet_root}/lib/ruby/site_ruby"
 
 "${puppet_root}/bin/ruby" -I"$ruby_path" "${puppet_root}/bin/puppet" apply \
-  --confdir "$confdir" --verbose \
+  --confdir "$confdir" --verbose --debug \
   "$@"
