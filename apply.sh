8ad688c0ff43ee58e96c2334a67727d79e7b93ef
David Banks <amoebae@gmail.com>
2018-11-29 09:55:33 +0000

    fix apply

--- apply.sh
+++ apply.sh
@@ -4,6 +4,11 @@ puppet_root=/opt/dissociated-puppet
 confdir="${puppet_root}/etc/puppet"
 ruby_path="${puppet_root}/lib/ruby/site_ruby"
 
+if "$#" -eq 0; then
+    echo "Need a manifest to apply." >&2
+    exit 1
+fi
+
 "${puppet_root}/bin/ruby" -I"$ruby_path" "${puppet_root}/bin/puppet" apply \
   --confdir "$confdir" --verbose --debug \
   "$@"
