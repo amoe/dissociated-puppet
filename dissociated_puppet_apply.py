fdd9831164258889c1f8eed5eaccc6e3a73f8274
David Banks <amoebae@gmail.com>
2018-11-29 10:38:32 +0000

    new stuff

--- dissociated_puppet_apply.py
+++ dissociated_puppet_apply.py
@@ -1,3 +1,23 @@
 #! /usr/bin/python3
 
-print("Hello")
+import subprocess
+import os
+import sys
+
+puppet_root = "/opt/dissociated-puppet"
+confdir = os.path.join(puppet_root, "etc/puppet")
+ruby_lib_path = os.path.join(puppet_root, "lib/ruby/site_ruby")
+ruby_bin_path = os.path.join(puppet_root, "bin/ruby")
+puppet_bin_path = os.path.join(puppet_root, "bin/puppet")
+
+manifest = sys.argv[1]
+
+subprocess.check_call([
+    ruby_bin_path,
+    "-I", ruby_lib_path,
+    puppet_bin_path,
+    "apply",
+    "--confdir", confdir,
+    "--verbose", "--debug",
+    manifest
+])
